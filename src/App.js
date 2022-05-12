07a78d414133fc64da640f8eb0b30b4cf95e4655
Calen Genobaga <calen.genobaga@yahoo.com>
2022-05-12 14:03:25 -0700

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import CampsitesList from './features/CampsitesList';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
-import Footer from './components/Footer'
-
+import Footer from './components/Footer';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='contact' element={<ContactPage/>} />
+          <Route path='directory' element={<CampsitesDirectoryPage/>} />
+      </Routes>
       <Footer />
     </div>
   );
